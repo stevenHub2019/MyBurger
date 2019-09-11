c3f2ec364a7c75f24ecb8ca8659e46720bd6ab8b
Steven <stevenzh09@gmail.com>
2019-09-11 12:47:21 +0800

    added Layout Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import Layout from './components/Layout/Layout'
 
 
 function App() {
   return (
     <div>
-      
+      <Layout>
+        <p>Burger Builder</p>
+      </Layout>      
     </div>
   );
 }
 
 export default App;
+
+
+
