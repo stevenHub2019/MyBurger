a50f18feccd2fe6c882e19e96f90aea24e51ae67
Steven <stevenzh09@gmail.com>
2019-10-02 16:20:43 +0800

    added lazy loading: dynamic loading

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React, {Component} from 'react';
+import React, {Component, Suspense} from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import {Route,Switch, Redirect} from 'react-router-dom';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
+//import Checkout from './containers/Checkout/Checkout';
+//import Orders from './containers/Orders/Orders';
+//import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 
 import {connect} from 'react-redux';
 import * as actionCreator from './store/actions/index';
+
+import Spinner from './components/UI/Spinner/Spinner';
+//dynamic import
+//Orders
+const Orders=React.lazy(()=>import('./containers/Orders/Orders'));
+//Checkout
+const Checkout=React.lazy(()=>import('./containers/Checkout/Checkout'));
+//Auth
+const Auth=React.lazy(()=>import('./containers/Auth/Auth'));
+
+
 
 
 class App extends Component {
@@ -25,8 +36,21 @@ class App extends Component {
     if(this.props.isAuth){
       route=(
         <Switch>
-          <Route path='/checkout' component= {Checkout}/> 
-          <Route path='/orders' component={Orders}/>
+          <Route path='/checkout' render={(props)=>(
+            <Suspense {...props} fallback={<Spinner/>}>
+              <Checkout {...props}/>
+            </Suspense>
+          )} /> 
+          <Route path='/orders' render={(props)=>(
+            <Suspense {...props} fallback={<Spinner/>}>
+              <Orders {...props}/>
+            </Suspense>
+          )}/>
+          <Route path='/auth' render={(props)=>(
+            <Suspense {...props} fallback={<Spinner/>}>
+              <Auth {...props}/>
+            </Suspense>
+          )}/>
           <Route path='/auth' component={Auth}/>
           <Route path='/logout' component={Logout}/>
           <Route path='/' component ={BurgerBuilder}/>
@@ -34,7 +58,11 @@ class App extends Component {
       )}else{
         route=(
           <Switch>
-            <Route path='/auth' component={Auth}/>
+            <Route path='/auth' render={(props)=>(
+              <Suspense {...props} fallback={<Spinner/>}>
+                <Auth {...props}/>
+              </Suspense>
+            )}/>
             <Route path='/' component ={BurgerBuilder}/>
             <Redirect to='/' />
           </Switch>
